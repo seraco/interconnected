d9f1e40de524cc17be2ceef4b555653b1e68e1ae
Sebastia Ramon <25517036+seraco@users.noreply.github.com>
2025-01-23 22:21:43 +0000

    Respond to user input

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,35 @@
-document.querySelectorAll(".card-label").forEach((element) => {
-  element.classList.add("lightseagreen");
+const solution = {
+  first: "easy",
+  second: "medium",
+  third: "hard",
+  fourth: "hardest",
+  fifth: "easy",
+  sixth: "medium",
+  seventh: "hard",
+  eighth: "hardest",
+  nineth: "easy",
+  tenth: "medium",
+  eleventh: "hard",
+  twelveth: "hardest",
+  thirteenth: "easy",
+  fourteenth: "medium",
+  fifteenth: "hard",
+  sixteenth: "hardest",
+};
+
+const selected = [];
+
+function inputHandler(e) {
+  console.log("Fired", e);
+  const parent = e.target.parentElement;
+  if (parent.classList.contains("lightseagreen")) {
+    e.target.parentElement.classList.remove("lightseagreen");
+    return;
+  }
+  e.target.parentElement.classList.add("lightseagreen");
+}
+
+const cards = document.querySelectorAll(".card-label");
+cards.forEach((element) => {
+  element.firstChild.addEventListener("input", inputHandler);
 });
