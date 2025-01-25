6b97c05863c9f1104d3b88991a983f31b012e0ea
Sebastia Ramon <25517036+seraco@users.noreply.github.com>
2025-01-25 16:00:13 +0000

    Able to navigate with keyboard

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -17,25 +17,57 @@ const solution = {
   sixteenth: "hardest",
 };
 
-function getCards() {
-  return document.querySelectorAll(".card-label");
+function getCardsContainer() {
+  return document.getElementById("board");
 }
 
-function updateCardsBackground() {}
+function getCards() {
+  return Array.from(document.querySelectorAll(".card-label"));
+}
+
+function getSelectedCards() {
+  return getCards().filter((card) => card.firstChild.checked);
+}
+
+getSelectedCards().forEach((card) => card.classList.add("selected"));
+
+getCardsContainer().addEventListener("input", inputHandler);
+
+function handleSelected(input) {
+  const card = input.parentNode;
+  const checked = input.checked;
+  if (!checked) {
+    card.classList.remove("selected");
+    return;
+  }
+  card.classList.add("selected");
+}
+
+function isWinningState() {
+  const selectedCards = getSelectedCards();
+  const areSameDifficulty = selectedCards.every((card) => {
+    const firstValue = selectedCards[0].firstChild.value.toLowerCase();
+    const value = card.firstChild.value.toLowerCase();
+    return solution[value] === solution[firstValue];
+  });
+  return areSameDifficulty && selectedCards.length === 4;
+}
+
+function updateBoardAfterWinningMove() {
+  const firstFourCards = getCards().slice(0, 4);
+  const selectedCards = getSelectedCards();
+  firstFourCards.forEach((card, idx) => {
+    const sibling = selectedCards[idx].nextSibling;
+    card.replaceWith(selectedCards[idx]);
+    sibling.parentNode.insertBefore(card, sibling);
+  });
+}
 
 function inputHandler(e) {
   console.log("Fired", e);
-  const parent = e.target.parentElement;
-  const checked = e.target.checked;
-  if (!checked) {
-    parent.classList.remove("lightseagreen");
-    return;
+  handleSelected(e.target);
+  if (isWinningState()) {
+    console.log(isWinningState());
+    updateBoardAfterWinningMove();
   }
-  parent.classList.add("lightseagreen");
 }
-
-getCards().forEach((element) => {
-  const child = element.firstChild;
-  if (child.checked) element.classList.add("lightseagreen");
-  child.addEventListener("input", inputHandler);
-});
